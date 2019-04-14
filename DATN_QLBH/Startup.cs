0332318b492d9cacbb4507a63077786f25107020
thanhkhoatran <35222306+thanhkhoatran@users.noreply.github.com>
2019-04-15 00:34:38 +0700

    Add project files.

---
+++ DATN_QLBH/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DATN_QLBH.Startup))]
+namespace DATN_QLBH
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
